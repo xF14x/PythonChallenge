5554d001968d3253f71b5fb28c3cfb5b432f5690
xF14x <>
2021-01-25 00:37:22 +0300

    Add the beginning of the program

--- PythonExam.py
+++ PythonExam.py
@@ -1,1 +1,25 @@
 # Programmit By Suliman Al-mohawis | Twitter - F14Commander Snapchat - sulimanxx1 YouTube - F14Commander
+from os import system
+from time import sleep
+system("clear")
+while True:
+    print("""
+EN:
+Hi
+This is a exam for Python ProgramLanguage
+it is for 
+
+AR:
+اهلا
+هذا البرنامج بيفيد مبرمجين لغة بايثون لمساعدتهم على زيادة سرعة الإنتباه
+للأخطاء اثناء كتاب الكود
+اتمنا بأن تستفيدون منه
+""")
+    print("")
+    print("If You The Finsh Red Enter (\"1\") ")
+    finsh = input("==> ")
+    if finsh =="1":
+        break
+    else:
+        pass
+    system("clear")
