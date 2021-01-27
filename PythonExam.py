abfdd66fe85f7155b000d337388da8e0021e5081
xF14x <>
2021-01-28 01:39:16 +0300

    finsh from The Esey Stage and start in The normal Stage

--- PythonExam.py
+++ PythonExam.py
@@ -50,12 +50,114 @@ hard(3)
 
 print("ok")
 sleep(2)
-system(clear)
-
+system("clear")
+points = 0
 if s =="1":
-    print(""" """)
+    print(""" 
+1-
+print("F14Commander: Hello")
+print("Pho1Yalfuhaid: hi")
+print("F14Commander: How are you @Pho1Yalfuhaid ?")
+brint("Pho1Yalfuhaid: fan thanls")
+
+2-
+print("F14Commander: Hello")
+prit("Pho1Yalfuhaid: hi")
+print("F14Commander: How are you @Pho1Yalfuhaid ?")
+print("Pho1Yalfuhaid: fan thanls")
+
+3- 
+print("F14Commander: Hello")
+print("Pho1Yalfuhaid: hi")
+print("F14Commander: How are you @Pho1Yalfuhaid ?")
+print("Pho1Yalfuhaid: fan thanls")
+
+""")
+    while True:
+        TheSolution = input("==> ")
+    
+        if TheSolution =="3":
+            points += 1
+            break
+        elif TheSolution == "1" or TheSolution == "2":
+            break
+        else:
+            pass
+    sleep(1)
+    system("clear")
+    print(""" 
+1-
+$name = "F14Commander"
+print(name)
+
+2-
+name = "F14Commander"
+print(name)
+
+3-
+name = "F14Commander"
+prlnt(name)
+
+""")
+    while True:
+
+        TheSolution = input("==> ")
+    
+        if TheSolution =="2":
+            points += 1
+            break
+        elif TheSolution == "1" or TheSolution == "3":
+            break
+        else:
+            pass
+    sleep(1)
+    system("clear")
+    print(""" 
+1-
+number = 3
+if name == 3:
+    print("your number is", number)
+
+2-
+number = "3"
+if number == 3:
+    print("your number is", number)
+
+-3
+number = 3
+if number = "3":
+    print("your number is", number)
+""")
+    while True:
+
+        TheSolution = input("==> ")
+    
+        if TheSolution =="1":
+            points += 1
+            break
+        elif TheSolution == "2" or TheSolution == "3":
+            break
+        else:
+            pass
+    print("")
+    print("your points is",points,"/ 3")
+    print("")
+    sleep(1)
 elif s =="2":
+    print(""" 
+
+""")
+    while True:
+            TheSolution = input("==> ")
+        
+            if TheSolution =="3":
+                points += 1
+                break
+            elif TheSolution == "1" or TheSolution == "2":
+                break
+            else:
+                pass
 
 elif s =="3":
-
+    pass
 else:
