aca3b7905ca6caf8e5b21097bee4e3f6d11eb345
xF14x <>
2021-01-27 20:18:26 +0300

    add The code befor rite The Questions

--- PythonExam.py
+++ PythonExam.py
@@ -1,6 +1,7 @@
 # Programmit By Suliman Al-mohawis | Twitter - F14Commander Snapchat - sulimanxx1 YouTube - F14Commander
 from os import system
 from time import sleep
+# =-=-=-=-=-=-=
 system("clear")
 while True:
     print("""
@@ -16,10 +17,45 @@ AR:
 اتمنا بأن تستفيدون منه
 """)
     print("")
-    print("If You The Finsh Red Enter (\"1\") ")
+    print("If You Finsh The Red Enter (\"1\") ")
     finsh = input("==> ")
     if finsh =="1":
         break
+        sleep(2)
     else:
         pass
     system("clear")
+# =-=-=-=-=-=-=-=-=
+while True:
+    system("clear")
+    
+    print("""
+Pleas slect:
+
+Esey(1)
+
+normal(2)
+
+hard(3)
+
+""")
+
+    s = input("==> ")
+    if s =="1" or s =="2" or s =="3":
+        break
+    else:
+        pass
+
+# =-=-=-=-=-=-=-=-=
+
+print("ok")
+sleep(2)
+system(clear)
+
+if s =="1":
+    print(""" """)
+elif s =="2":
+
+elif s =="3":
+
+else:
