bea3f5e7d73551de8f85d7bee5a4b5f57c80cf43
xF14x <>
2021-01-28 17:50:40 +0300

    finsh from The Normal Stage and start in The hard Stage

--- PythonExam.py
+++ PythonExam.py
@@ -1,6 +1,7 @@
 # Programmit By Suliman Al-mohawis | Twitter - F14Commander Snapchat - sulimanxx1 YouTube - F14Commander
 from os import system
 from time import sleep
+from typing import get_origin
 # =-=-=-=-=-=-=
 system("clear")
 while True:
@@ -23,6 +24,7 @@ AR:
         break
         sleep(2)
     else:
+        print("Pleas select from 1 To 3")
         pass
     system("clear")
 # =-=-=-=-=-=-=-=-=
@@ -44,6 +46,7 @@ hard(3)
     if s =="1" or s =="2" or s =="3":
         break
     else:
+        print("Pleas select from 1 To 3")
         pass
 
 # =-=-=-=-=-=-=-=-=
@@ -52,6 +55,7 @@ print("ok")
 sleep(2)
 system("clear")
 points = 0
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 if s =="1":
     print(""" 
 1-
@@ -82,6 +86,7 @@ print("Pho1Yalfuhaid: fan thanls")
         elif TheSolution == "1" or TheSolution == "2":
             break
         else:
+            print("Pleas select from 1 To 3")
             pass
     sleep(1)
     system("clear")
@@ -109,6 +114,7 @@ prlnt(name)
         elif TheSolution == "1" or TheSolution == "3":
             break
         else:
+            print("Pleas select from 1 To 3")
             pass
     sleep(1)
     system("clear")
@@ -128,6 +134,7 @@ number = 3
 if number = "3":
     print("your number is", number)
 """)
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     while True:
 
         TheSolution = input("==> ")
@@ -138,26 +145,186 @@ if number = "3":
         elif TheSolution == "2" or TheSolution == "3":
             break
         else:
+            print("Pleas select from 1 To 3")
             pass
     print("")
     print("your points is",points,"/ 3")
     print("")
     sleep(1)
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 elif s =="2":
     print(""" 
+1-
+name "suliman"
+if name == "suliman":
+    print("welcome back")
+else:
+    print("Error")
+2-
+name = suliman
+if name == suliman:
+    print("welcome back")
+else:
+    print("Error")
 
+3-
+name = "suliman"
+if name === suliman :
+    print("welcome back")
+else:
+    print("Error")
 """)
     while True:
             TheSolution = input("==> ")
         
-            if TheSolution =="3":
+            if TheSolution =="1":
                 points += 1
                 break
-            elif TheSolution == "1" or TheSolution == "2":
+            elif TheSolution == "2" or TheSolution == "3":
                 break
             else:
+                print("Pleas select from 1 To 3")
                 pass
+    sleep(1)
+    system("clear")
+    print(""" 
+1-
+number = 0
+number + 65
+print(number)
 
+2-
+number + 65
+
+3-
+number = 0
+number += 65
+""")
+    while True:
+        TheSolution = input("==> ")
+        
+        if TheSolution =="3":
+            points += 1
+            break
+        elif TheSolution == "1" or TheSolution == "2":
+            break
+        else:
+            print("Pleas select from 1 To 3")
+            pass
+    sleep(1)
+    system("clear")
+    print("""
+1-
+name = "suliman"
+if name == "suliman" :
+    print("Welcome", name)
+
+2-
+if name == "suliman":
+    print("welcome", name)
+3-
+nname = "suliman"
+if name == "suliman":
+    print("welcome", name)
+""")
+    while True:
+        TheSolution = input("==> ")
+        
+        if TheSolution =="2":
+            points += 1
+            break
+        elif TheSolution == "1" or TheSolution == "3":
+            break
+        else:
+            print("Pleas select from 1 To 3")
+            pass
+    sleep(1)
+    system("clear")
+    print(""" 
+1-
+from os import system
+print("welcome back")
+syste("clear")
+name = "suliman"
+if name == "suliman":
+    print("hi", name)
+
+2-
+from os import system
+print("welcome back")
+system("clear")
+name = "suliman"
+if name == "sulimn":
+    print("hi", name)
+
+3-
+from os import system
+print("welcome back")
+system("clear")
+name = "suliman"
+if name == "suliman":
+    print("hi", name)
+""")
+    while True:
+        TheSolution = input("==> ")
+        
+        if TheSolution =="3":
+            points += 1
+            break
+        elif TheSolution == "1" or TheSolution == "2":
+            break
+        else:
+            print("Pleas select from 1 To 3")
+            pass
+    sleep(1)
+    system("clear")
+    print(""" 
+1-
+from time import sleep
+from sys import *
+from os import system
+
+os.system("clear")
+time.sleep(3)
+name = "hi"
+print(name)
+
+2-
+from time import sleep
+from sys import *
+from os import system
+
+system("clear")
+time.slee
+name = "hi"
+print(name)
+
+3-
+from time import sleep
+from sys import *
+from os import system
+
+system("clear")
+sleep(3)
+name = "hi"
+print(name)
+""")
+    while True:
+        TheSolution = input("==> ")
+        
+        if TheSolution =="3":
+            points += 1
+            break
+        elif TheSolution == "1" or TheSolution == "2":
+            break
+        else:
+            print("Pleas select from 1 To 3")
+            pass
+    print("")
+    print("your points is",points,"/ 5")
+    print("")
+    sleep(1)
+    
 elif s =="3":
     pass
 else:
